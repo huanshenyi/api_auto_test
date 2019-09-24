1598da686a0275855eafe6f6c6c3713831575e3a
huanshenyi <txy1226052>
2019-09-24 17:16:09 +0900

    deepdiff->jsonの形式判断用

--- Run/run_main.py
+++ Run/run_main.py
@@ -6,6 +6,7 @@ rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
 
 from Util.handle_excel import excel_data
+from Util.handel_result import handel_result_json
 from Util.handel_result import handel_result
 from Base.base_request import request
 
@@ -17,21 +18,34 @@ class RunMain:
         rows = excel_data.get_rows()
         for i in range(rows):
             data = excel_data.get_rows_value(i + 2)
-            # print(data)
             is_run = data[2]
-            # print(is_run)
             if is_run == "yes":
                 method = data[5]
                 url = data[4]
                 data1 = data[6]
+                # 予想結果判断の方法
+                excepect_method = data[8]
+                # 予想結果
+                excepect_result = data[9]
                 res = request.run_main(method, url, data1)
                 # 結果と予想結果を比較する
                 # サーバーのerrorCode
-                code = res["errorCode"]
+                code = str(res["errorCode"])
                 message = res["errorDesc"]
-                cofig_message = handel_result(url, str(code))
-                print(message, cofig_message)
-                print(res)
+                if excepect_method == "mec":
+                    config_message = handel_result(url, str(code))
+                    if message == config_message:
+                        print("case通過")
+                    else:
+                        print("case失敗")
+                if excepect_method == "errorcode":
+                    if excepect_result == code:
+                        print("テスト通過")
+                    else:
+                        print("テスト失敗")
+                if excepect_method == "json":
+                    # handel_result_json()
+                    pass
                 # break
 
 
--- Util/handel_result.py
+++ Util/handel_result.py
@@ -6,6 +6,7 @@ rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
 import configparser
 import json
+from deepdiff import DeepDiff
 from Util.handle_json import get_value
 
 # get_value("api3/getbanneradvertver2", "/Config/code_message.json")
@@ -24,6 +25,20 @@ def handel_result(url, code) -> any:
     return None
 
 
+def handel_result_json():
+    """
+    形式の確認
+    :return:
+    """
+    dict1 = {"aaa": "AA1A", "bbb": "BB1B1", "DDD": [{"12", "22"}, {"35", "14"}]}
+    dict2 = {"aaa": "AAA", "bbb": "BB1B", "DDD": [{"12", "22"}, {"35", "14"}]}
+    cmp_dict = DeepDiff(dict1, dict2, ignore_order=True).to_dict()
+    if cmp_dict.get("dictionary_item_added"):
+        print("case失敗")
+    else:
+        print("case成功")
+
 
 if __name__ == "__main__":
-   print(handel_result("api3/getbanneradvertver2", "10001"))
+   # print(handel_result("api3/getbanneradvertver2", "10001"))
+   handel_result_json()
